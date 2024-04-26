688722b1f5578b5b5b8edd908fb345659a015eef
Nabemh <nhanior@gmail.com>
2024-04-26 16:03:37 +0100

    initail practice

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/*
+ * Description: Printing input entered by the user in a java program
+ * 
+ */
+
+
+ class Main {
+    public static void main(String[] args) {
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+
+        System.out.println("Enter your name and age: ");
+        String myName = input1.nextLine();
+        int myAge = input2.nextInt();
+
+        System.out.printf("Your name is %s and you are %d years old.\n", myName, myAge);
+
+    }
+ }
---
+++ me_class.java
@@ -0,0 +1,29 @@
+/*
+ * Me.sayName - prints out the name of user
+ * Me.sayAge - prints out the age of user
+ *
+ * Return: Non-applicable
+ */
+
+
+class Me {
+	static void sayName()
+	{
+		System.out.println("My name is: User");
+	}
+
+	static void sayAge()
+	{
+		System.out.println("Age = 15");
+	}
+}
+
+class Main {
+	public static void main(String[] args)
+	{
+		Me.sayName();
+		Me.sayAge();
+
+		System.out.println("Not bad for a first trial");
+	}
+}
