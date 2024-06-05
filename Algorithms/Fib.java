09204bfcb977d8c8af9555b717179f06560b67ff
Nabemh <nhanior@gmail.com>
2024-06-05 10:27:12 +0100

    Algorithms and ds

---
+++ Algorithms/Fib.java
@@ -0,0 +1,21 @@
+package Algorithms;
+
+public class Fib {
+    public static void main(String[] args) {
+        int first = 0;
+        int second = 1;
+        int next;
+        System.out.println(first);
+        System.out.println(second);
+
+        for (int i = 2; i < 10; i++){
+            next = first + second;
+            System.out.println(next);
+            first = second;
+            second = next;
+        }
+    }
+
+
+}
+
---
+++ Algorithms/Swap.java
@@ -0,0 +1,12 @@
+package Algorithms;
+
+public class Swap {
+    public static void main(String[] args) {
+        int number = 78;
+        int anotherNumber = 32;
+
+        int temp = number;
+        number = anotherNumber;
+        anotherNumber = temp;
+    }
+}
