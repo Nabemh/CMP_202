edb82069d45c20d41de9d1670c78715604ed87a3
Nabemh <nhanior@gmail.com>
2024-06-13 14:11:09 +0100

    loop modified

--- Arrays/practice/two_sum.java
+++ Arrays/practice/two_sum.java
@@ -1,18 +1,18 @@
-package Arrays;
+package Arrays.practice;
 
 public class two_sum {
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 3, 4, 5};
-        int target = 6;
+        int target = 9;
 
         for (int i = 0; i < nums.length; i++) {
-            for (int n = 0; n < nums.length; n++){
+            for (int n = i + 1; n < nums.length; n++){
                 int check = nums[i] + nums[n];
                 if (check == target){
                     System.out.printf("Output: [%d, %d]", nums[i], nums[n]);
                     break;
                 }
-            }
+             }
         }
     }
 }
